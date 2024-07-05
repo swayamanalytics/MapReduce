6d18ea8ccabb303f8a1caee0cb37439cffb42035
unknown <swayam.analytics@gmail.com>
2024-07-05 21:59:09 +0530

    MapReduceFiles

---
+++ WordCountPartitioner.java
@@ -0,0 +1,15 @@
+package org.kelly.hadoop.mr.jobs;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class WordCountPartitioner extends Partitioner<Text, LongWritable>{
+
+	@Override
+	public int getPartition(Text key, LongWritable value, int noOfReducers) {
+		String tempString = key.toString();
+		return (tempString.toLowerCase().charAt(0)-'a')%noOfReducers;
+	}
+
+}
